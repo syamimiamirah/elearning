22734f77ca0310eb95aa3fd06bd43338a738d10a
syamimiamirah <syamimiamirah@graduate.utm.my>
2025-01-18 19:34:41 +0800

    add footer and navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer';
+import Nav from './Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Nav/>
+     <Routes>
+      <Route path="/" element={<h1>Home Component</h1>}/>
+      <Route path="/events" element={<h1>Events Component</h1>}/>
+      <Route path="/pomodoro" element={<h1>Pomodoro Component</h1>}/>
+      <Route path="/profile" element={<h1>Profile Component</h1>}/>
+      <Route path="/logout" element={<h1>Logout Component</h1>}/>
+      </Routes>
+      </BrowserRouter>
+      <Footer/>
+     
     </div>
   );
 }
---
+++ frontend/src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav=()=>{
+    return(
+        <div>
+            <ul className='nav-ul'>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/events">Events</Link></li>
+                <li><Link to="/pomodoro">Pomodoro</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+                <li><Link to="/logout">Logout</Link></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
